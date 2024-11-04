2b238dbaf02d6471b4879070d2fa0ddf33cf552e
Pelayogm <pelagrmenendez12@gmail.com>
2024-11-04 21:44:35 +0100

    V0.2 Ejercicio 1

--- React/ejerciciocomponentessencillosreact/src/App.js
+++ React/ejerciciocomponentessencillosreact/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Ejercicio_1 from './Ejercicio_1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Componentes Sencillos Pelayogm</p>
       </header>
+      <body className="App-header">
+        <p>Ejercicio_1</p>
+        <Ejercicio_1/>
+      </body>
     </div>
   );
 }
---
+++ React/ejerciciocomponentessencillosreact/src/Ejercicio_1.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function Ejercicio_1() {
+  const [contador, setContador] = useState(0);
+
+  return (
+    <div>
+      <p>Contador: {contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+      <button onClick={() => setContador(contador - 1)}>Decrementar</button>
+    </div>
+  );
+}
+export default Ejercicio_1;
